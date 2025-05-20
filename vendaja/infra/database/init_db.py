d9ba526ae64d204acf035796fb444a61b0f63767
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-19 22:26:52 -0300

    chore: import all models in init_db file to create all when executed

--- vendaja/infra/database/init_db.py
+++ vendaja/infra/database/init_db.py
@@ -6,6 +6,9 @@ import asyncio
 async def init_models():
     async with engine.begin() as conn:
         from ...models.store import StoreModel
+        from ...models.sale import SaleModel
+        from ...models.product import ProductModel
+        from ...models.receipt import ReceiptModel
         await conn.run_sync(Base.metadata.create_all)
 
 asyncio.run(init_models())
