e2b28649cf80daaeaf4b5a8b59b60ba9062beb8a
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-22 17:08:39 -0300

    refactor: transforming storeservice into a class and updating its creation method; creating exceptions file

--- vendaja/services/store.py
+++ vendaja/services/store.py
@@ -1,42 +1,39 @@
-from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from http import HTTPStatus
 
 from ..schemas.store import CreateStoreSchema
 from ..models.store import StoreModel
 
 from ..utils.security.data_processing import encrypt_fields
+from .exceptions import StoreAlreadyRegistered
 
-sensive_fields = ['document', 'picpay_client_id', 'picpay_client_secret', 'whatsapp_token']
 
-async def create_store(store: CreateStoreSchema, session: AsyncSession):
-    encrypt_fields(store, sensive_fields)
-    conditions = (
-        (StoreModel.email == store.email) |
-        (StoreModel.phone == store.phone) |
-        (StoreModel.document == store.document) |
-        (StoreModel.pipcay_client_id == store.pipcay_client_id) |
-        (StoreModel.picpay_client_secret == store.picpay_client_secret) |
-        (StoreModel.whatsapp_token == store.whatsapp_token)
-    
-    )
-    db_store = await session.scalar(select(StoreModel).where(conditions))
-    if db_store:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Alguma informação já consta no sistema.'
+
+class StoreService:
+    def __init__(self, session: AsyncSession):
+        self.__session = session
+        self.__sensive_fields = ['document', 'picpay_client_id', 'picpay_client_secret', 'whatsapp_token']
+
+    async def create_store(self, store: CreateStoreSchema):
+        encrypt_fields(store, self.__sensive_fields)
+        conditions = (
+            (StoreModel.email == store.email) |
+            (StoreModel.phone == store.phone) |
+            (StoreModel.document == store.document) |
+            (StoreModel.pipcay_client_id == store.pipcay_client_id) |
+            (StoreModel.picpay_client_secret == store.picpay_client_secret) |
+            (StoreModel.whatsapp_token == store.whatsapp_token)
         )
-    try:
-        db_store = StoreModel(
+
+        existing_store = await self.__session.scalar(select(StoreModel).where(conditions))
+        if existing_store:
+            raise StoreAlreadyRegistered('Store already exists')
+
+        new_store = StoreModel(
             **store.model_dump()
         )
-        await session.add(db_store)
-        await session.commit()
-        await session.refresh(db_store)
-    except Exception as e:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail=f'{e}'
-        )
+        self.__session.add(new_store)
+        await self.__session.commit()
+        await self._session.refresh(new_store)
 
+        return new_store
