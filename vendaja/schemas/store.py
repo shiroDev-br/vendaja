f35e40f49cb4284b6e164e4675f5842dc49dec3e
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-22 23:37:08 -0300

    refactor: resolving error in creating the bank session and removing created_at from the store schema

--- vendaja/schemas/store.py
+++ vendaja/schemas/store.py
@@ -1,9 +1,7 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
+from pydantic import BaseModel
 
 class CreateStoreSchema(BaseModel):
     name: str
     document: str
     email: str
     phone: str
-    created_at: datetime = Field(default_factory=datetime.now)
