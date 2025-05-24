a3325447d4c4059678cdfc9b77b5eba0803c56fd
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-24 10:55:17 -0300

    chore: generate a migration for remove a uniqueconstraint of name in productmodel

--- vendaja/services/product.py
+++ vendaja/services/product.py
@@ -5,14 +5,22 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
+from uuid import uuid
+
 from ..infra.database.session import get_session
 
 from ..schemas.product import CreateProductSchema
 from ..models.product import ProductModel
 
-from .exceptions import ProductAlreadyExists
 
 class ProductService:
     def __init__(self, session: AsyncSession):
         self.session = session
-        
+    
+    async def create_product(
+        self,
+        product: CreateProductSchema,
+        store_id: uuid
+    ) -> ProductModel:
+
+        product.store_id = store_id
